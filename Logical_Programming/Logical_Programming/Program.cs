e24a394f4487f4694855e2cfb16b8cdec4456070
Ruchitachaudhari16 <ruchitachaudhari70@gmail.com>
2023-02-25 17:28:11 -0500

    [Ruchita] Add: elapsed time is measure

--- Logical_Programming/Logical_Programming/Program.cs
+++ Logical_Programming/Logical_Programming/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Reverseno.Check();
+            StopWatch.Time_Check();
         }
     }
 }
---
+++ Logical_Programming/Logical_Programming/StopWatch.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logical_Programming
+{
+    internal class StopWatch
+    {
+        public static void Time_Check()
+        {
+            
+            Console.WriteLine("Press any key to start the stopwatch");
+            Console.ReadKey();
+            DateTime startTime= DateTime.Now;
+            Console.WriteLine("Stopwatch started.Press any key to stop");
+            Console.ReadKey();
+            DateTime stopTime = DateTime.Now;
+            TimeSpan elapsedTime = stopTime - startTime;
+            Console.WriteLine("Elapsed time: {0}", elapsedTime);
+        }
+    }
+}
