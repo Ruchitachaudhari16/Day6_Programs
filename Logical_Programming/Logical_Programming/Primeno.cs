d2d4f13b329a8a4dac98ee19b7b9d4db6af618be
Ruchitachaudhari16 <ruchitachaudhari70@gmail.com>
2023-02-25 15:42:55 -0500

    [Ruchita] Add: Prime Number Program

---
+++ Logical_Programming/Logical_Programming/Primeno.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logical_Programming
+{
+    internal class Primeno
+    {
+        public static void PrimeCheck()
+        {
+            Console.WriteLine("Enter the number weather it is prime or not");
+            int number=Convert.ToInt32(Console.ReadLine());
+            bool ifEnterCheck = false;
+            for (int i = 2; i < number ; i++)
+            {
+                if(number % i == 0)
+                {
+                    Console.WriteLine("Number is not a Prime");
+                    ifEnterCheck = true;
+                    break;
+                }
+            }
+            if (ifEnterCheck == false)
+            {
+                Console.WriteLine("Number is a Prime");
+            }
+        }
+    }
+}
